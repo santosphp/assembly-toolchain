50ebd002fdd7f5f49083a950e38dad68d408e470
santosphp <94130906+santosphp@users.noreply.github.com>
2025-06-17 21:53:46 -0300

    Add base MainFrame class

--- src/toolchain/gui/MainFrame.java
+++ src/toolchain/gui/MainFrame.java
@@ -1,10 +1,23 @@
 package toolchain.gui;
 
-public class MainFrame {
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
+public class MainFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+    public MainFrame() {
+        super("Educational Assembly Toolchain");
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);  // Close application on X
+        setLocationRelativeTo(null); // Center on screen
+    }
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+        SwingUtilities.invokeLater(() -> {
+            new MainFrame().setVisible(true);
+        });
 	}
 
 }
